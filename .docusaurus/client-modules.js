a8e02177a8608196fdaef5c97a0a51becdbc4d0e
Muhammad Jamshaid Aziz <jamshaid.aziz121@gmail.com>
2026-02-07 19:30:00 +0500

    front_end

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("/mnt/d/piaic/Regeneration_01/aziz_restuarant/frontend/node_modules/infima/dist/css/default/default.css"),
-  require("/mnt/d/piaic/Regeneration_01/aziz_restuarant/frontend/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/mnt/d/piaic/Regeneration_01/aziz_restuarant/frontend/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/mnt/d/piaic/Regeneration_01/aziz_restuarant/frontend/src/styles/globals.css"),
+  require("/mnt/d/piaic/Regeneration_01/Aziz_Restuarant/frontend/node_modules/infima/dist/css/default/default.css"),
+  require("/mnt/d/piaic/Regeneration_01/Aziz_Restuarant/frontend/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/mnt/d/piaic/Regeneration_01/Aziz_Restuarant/frontend/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/mnt/d/piaic/Regeneration_01/Aziz_Restuarant/frontend/src/styles/globals.css"),
 ];
